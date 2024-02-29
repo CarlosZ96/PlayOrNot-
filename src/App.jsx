d91f904ad6791d56bc038aaddd8b382dfb4dde2b
CarlosZ96 <c4r1oszambrano@gmail.com>
2024-02-29 13:20:30 -0500

    Update App.jsx
    Fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
 import './App.css';
 import MainPage from './components/MainPage';
-import { Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Cambiado a BrowserRouter
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-
+        <div>
+          <h1>La tatuateria</h1>
+        </div>
       </header>
-      <Routes>
-        <Route path="/" element={<MainPage />} />
-      </Routes>
+      <Router> 
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+        </Routes>
+      </Router> {}
     </div>
   );
 }
 
 export default App;
+
