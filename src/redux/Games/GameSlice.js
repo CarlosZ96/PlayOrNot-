ce3a6611960e8fda33d2b6a0f1a1373eb6e7d9d1
CarlosZ96 <c4r1oszambrano@gmail.com>
2024-03-11 21:21:20 -0500

    Update GameSlice.js
    fix

--- src/redux/Games/GameSlice.js
+++ src/redux/Games/GameSlice.js
@@ -7,27 +7,17 @@ export const getReleases = createAsyncThunk(
     try {
       const releases = [];
       const currentDate = new Date('2024-03-10');
-      for (let id = 1; id <= 864300; id++) {
+      for (let id = 100000; id <= 864300; id++) {
         try {
           const response = await axios.get(`https://api.rawg.io/api/games/${id}?key=f40cb22a32854188aa4cbf6538242b50`);
           const agame = response.data;
-          const name = agame.name;
           const idg = agame.id;
-          const metacritic = agame.metacritic;
-          const background_image = agame.background_image;
-          const description = agame.description;
           const release_date = agame.released;
-          const parent_platforms = agame.parent_platforms.map(platform => platform.platform.name); // Assuming you want platform names only
           const gameReleaseDate = new Date(agame.released); 
           if (gameReleaseDate < currentDate && gameReleaseDate.getFullYear() === 2024) {
             releases.push({
               idg,
-              name,
-              metacritic,
-              background_image,
               release_date,
-              description,
-              parent_platforms,
             }); 
           }
         } catch (error) {
