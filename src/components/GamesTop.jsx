dc265c31a60114063d912b407134d0533b9123e4
CarlosZ96 <c4r1oszambrano@gmail.com>
2024-03-20 00:03:44 -0500

    Create GamesTop.jsx

---
+++ src/components/GamesTop.jsx
@@ -0,0 +1,10 @@
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getReleases } from '../redux/Games/GameSlice';
+import { nanoid } from 'nanoid';
+
+function GamesTop()  {
+
+}
+
+export default GamesTop;
