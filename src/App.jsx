31163521474cc80b792e45a3613f579b6680417a
CarlosZ96 <c4r1oszambrano@gmail.com>
2024-03-22 19:53:46 -0500

    Feature Search:
    -useeffect and you state to continuously change the value that the user types to search for a game using the https request using that entered value as the get parameter in the body of the request.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import './App.css';
+import React, { useState, useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import MainPage from './components/MainPage';
 import Search from './img/search.png';
@@ -6,6 +6,40 @@ import LogIn from './img/Muhamad Ulum.png';
 import './stylesheets/app.css';
 
 function App() {
+  const [gameName, setGameName] = useState('');
+  const [games, setGames] = useState([]);
+  const url = 'http://localhost:8080/https://api.igdb.com/v4/games/';
+  const headers = {
+    'Client-ID': 'jeqorghffhp2lzx25w4hjazivbkahe',
+    'Authorization': 'Bearer yol7xd1r00hd58t8i081u1a2yzjcsm',
+    'Content-Type': 'text/plain',
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url, {
+          method: 'POST',
+          headers: headers,
+          body: `fields *; where name ~ *"${gameName}"*; limit 5;`
+        });
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const findGames = await response.json();
+        setGames(findGames);
+      } catch (error) {
+        console.error('There was a problem with fetch operation:', error);
+      }
+    };
+
+    fetchData();
+  }, [gameName]);
+
+  const handleInputChange = (e) => {
+    setGameName(e.target.value);
+  };
+
   return (
     <div className="App">
       <header className='header-container'>
@@ -20,17 +54,29 @@ function App() {
           </div>
         </div>
         <div className='search-container'>
-          <input type="text" className="searchi" placeholder="  Search.."/>
+          <input
+            type="text"
+            className="searchi"
+            placeholder="  Search.."
+            value={gameName}
+            onChange={handleInputChange}
+          />
           <button className='search-button'><img src={Search} alt="" className='search' /></button>
           <button className='LogIn'><img src={LogIn} alt="" className='Mar' /></button>
+          <ul className='find-games-container'>
+            {games.map(game => (
+              <li key={game.id}>
+                <span>{game.name}</span>
+              </li>
+            ))}
+          </ul>
         </div>
       </header>
       <Routes>
-        <Route path="/" element={<MainPage />} />
+        <Route path="/" element={<MainPage games={games} />} />
       </Routes>
     </div>
   );
 }
 
 export default App;
-
