2d0300a85f8dcf2baf3f74877883ff641dab49ed
CarlosZ96 <c4r1oszambrano@gmail.com>
2024-06-05 10:38:11 -0500

    Update MediaButtons.jsx

--- src/components/reviews/media/MediaButtons.jsx
+++ src/components/reviews/media/MediaButtons.jsx
@@ -3,30 +3,22 @@ import PropTypes from 'prop-types';
 import styles from '../../../stylesheets/reviews.module.css';
 
 const MediaButtons = ({ showMediaSection }) => {
-  const screenshotsRef = useRef();
-  const videosRef = useRef();
-  const dlcRef = useRef();
 
-  const handleMediaSectionToggle = (ref) => {
-    const container = ref.current;
-    if (container) {
-      const buttons = [screenshotsRef, videosRef, dlcRef];
-      buttons.forEach((btnRef) => {
-        if (btnRef.current !== ref.current) {
-          btnRef.current.classList.remove(styles['selected']);
-          btnRef.current.classList.add(styles['unselected']);
-        }
-      });
-      container.classList.remove(styles['unselected']);
-      container.classList.add(styles['selected']);
-    }
+
+  const handleMediaSectionToggle = (section) => {
+    const screenshotsRef = useRef();
+    const videosRef = useRef();
+    const dlcRef = useRef();
+    if (section === 'selected') {
+      container.classList.remove(styles.hide);
+    } 
   };
 
   return (
     <div className={styles['game-extra-content-txt-container']}>
-      <div onClick={() => { showMediaSection('screenshots'); handleMediaSectionToggle(screenshotsRef); }} ref={screenshotsRef} className={styles['selected']}>Screenshots</div>
-      <div onClick={() => { showMediaSection('videos'); handleMediaSectionToggle(videosRef); }} ref={videosRef} className={styles['unselected']}>Videos</div>
-      <div onClick={() => { showMediaSection('dlc'); handleMediaSectionToggle(dlcRef); }} ref={dlcRef} className={styles['unselected']}>DLC</div>
+      <div onClick={() => { showMediaSection('screenshots'); handleMediaSectionToggle(handleMediaSectionToggle); }} ref={screenshotsRef} className={styles['selected']}>Screenshots</div>
+      <div onClick={() => { showMediaSection('videos'); handleMediaSectionToggle(handleMediaSectionToggle); }} ref={videosRef} className={styles['unselected']}>Videos</div>
+      <div onClick={() => { showMediaSection('dlc'); handleMediaSectionToggle(handleMediaSectionToggle); }} ref={dlcRef} className={styles['unselected']}>DLC</div>
     </div>
   );
 };
